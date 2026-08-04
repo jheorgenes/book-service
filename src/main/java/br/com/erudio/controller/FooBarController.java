0be3ef7b0002d817746941237832cad109b6da4b
Jheorgenes Warlley <jheorgenes@gmail.com>
2026-08-04 20:40:46 -0300

    - Aprendendo várias formas de controlar os acesso as requisições, como RateLimiter, Retry, Bulkhead e Circuit Breaker - Aprendendo como definir as anotações no controller e como configurar no application.yml.

--- src/main/java/br/com/erudio/controller/FooBarController.java
+++ src/main/java/br/com/erudio/controller/FooBarController.java
@@ -1,5 +1,8 @@
 package br.com.erudio.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +18,20 @@ public class FooBarController {
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    @Retry(name = "foo-bar") //Declarando que esse método será observado pelo Retry e tentatá chamá-lo a quantidade de vezes especificada pelo resilience4j max-attempts
+//    @Retry(name = "foo-bar") //Declarando que esse método será observado pelo Retry e tentatá chamá-lo a quantidade de vezes especificada pelo resilience4j max-attempts
+//    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") //Em caso de falha, chamará esse método abaixo cujo o nome é o mesmo declarado aqui
+//    @CircuitBreaker(name = "foo-bar", fallbackMethod = "fallbackMethod")
+//    @RateLimiter(name = "default") //Limitando a quantidade de requisições permitidas
+    @Bulkhead(name = "default")
     public String fooBar() {
         logger.info("Request to foo-bar is received!");
-        // Simulando uma requisição para uma rota que não existe, para falhar e observar o comportameno dessa falha no circuit breaker
-        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
-        return response.getBody();
+//        // Simulando uma requisição para uma rota que não existe, para falhar e observar o comportameno dessa falha no circuit breaker
+//        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+//        return response.getBody();
+        return "Foo Bar!!!";
+    }
+
+    public String fallbackMethod(Exception ex) {
+        return "fallbackMethod foo-bar!!!";
     }
 }
